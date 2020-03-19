0f8e03d0461a7ea08d0401e5ad977a91d7fed030
ArsenyAdwsdas <61195101+ArsenyAdwsdas@users.noreply.github.com>
2020-03-19 22:20:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -242,7 +242,7 @@ var dima_server = 621728738316386344
         if (arguments[0]) {
           if(message.content.startsWith(prefix + 'Move Role')){let fsayufh = arguments.shift()}
           message.mentions.roles.first().setPosition(parseInt(arguments[0]), true)
-          message.channel.send(parseInt(arguments[0]))
+          message.channel.send(arguments[0])
           if (message.content.startsWith(prefix + 'Move Role-g') || message.content.startsWith(prefix + 'MR-g')) {send_to = message.channel} else {send_to = message.author}
           if (!message.content.startsWith(prefix + 'Move Role-s') || !message.content.startsWith(prefix + 'MR-s')){send_to.send("Роль передвинута")}
         } else {message.channel.send("А КАК СДВИНУТЬ? (+x это -- а -x это --)")}
